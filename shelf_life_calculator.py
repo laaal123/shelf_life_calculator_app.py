b728bde38136cc3fad77f39dc472c29cbf6d1696
laaal123 <127331559+laaal123@users.noreply.github.com>
2025-07-09 19:29:23 +0600

    Update shelf_life_calculator.py

--- shelf_life_calculator.py
+++ shelf_life_calculator.py
@@ -80,10 +80,13 @@ def determine_shelf_life(
                 result["Decision"] = "Up to 1.5x (max +6 M)"
                 result["Notes"] = "Backed by statistical analysis and supporting data"
                 return result
-            else:
-                 result["Proposed Shelf Life (Y)"] = x_months + 3
-                 result["Decision"] = "Up to +3 M"
-                 result["Notes"] = "Based on relevant supporting data and regression supports this extrapolation"
+    if sig_change_6m_accel:
+        if sig_change_3m_accel:
+            if not long_term_stats_amenable or not supporting_data_available and stats_performed:
+                result["Proposed Shelf Life (Y)"] = min(x_months * 1.5, x_months + 6)
+                result["Decision"] = "Up to 1.5x (max +6 M)"
+                result["Notes"] = "Backed by statistical analysis and supporting data"
+                return result
            
 
     if not sig_change_6m_accel:
