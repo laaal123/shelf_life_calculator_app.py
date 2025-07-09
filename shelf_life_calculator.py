229fa8649852af4a137935b7739364da8d415355
laaal123 <127331559+laaal123@users.noreply.github.com>
2025-07-09 19:26:36 +0600

    Update shelf_life_calculator.py

--- shelf_life_calculator.py
+++ shelf_life_calculator.py
@@ -84,10 +84,7 @@ def determine_shelf_life(
                  result["Proposed Shelf Life (Y)"] = x_months + 3
                  result["Decision"] = "Up to +3 M"
                  result["Notes"] = "Based on relevant supporting data and regression supports this extrapolation"
-            else:
-                 result["Decision"] = "No extrapolation"
-                 result["Notes"] = "Regression does not support the proposed +3 M extrapolation"
-                 return result
+           
 
     if not sig_change_6m_accel:
         if not change_long_term_data:
